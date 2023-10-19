3e9c5cf627075dc21dd0e6a21ba01979659c0691
aleWidomlanski <widomlanski@hotmail.com>
2023-10-18 21:40:01 -0300

    refactorizando codigo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,68 @@
-"use client"
+"use client";
 import { useForm } from "react-hook-form";
-import { Button } from "@/components/atoms";
-import { InputLabel } from "@/components/molecules";
+import { Button, Input } from "@/components/atoms";
+import { InputContainer } from "@/components/atoms/InputContainer";
 import { loginUser } from "../../services/login";
 import { OptionsInputs } from "@/components/molecules/OptionsInputs";
 import styles from "./page.module.scss";
 
-
 const validationEmail = {
   required: {
     value: true,
-    message: "Email es requerido"
+    message: "Email es requerido",
   },
   pattern: {
-    value: /^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/i,
-    message: "Formato de email invalido"
-  } 
-}
-
+    value:
+      /^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/i,
+    message: "Formato de email invalido",
+  },
+};
 
 const validationPassword = {
   required: {
     value: true,
-    message: "Password es requerido"
+    message: "Password es requerido",
   },
   minLength: {
-    value: 10,
-    message: "Password debe contener 10 carácteres de minimo"
+    value: 6,
+    message: "El Password debe contener 6 carácteres como minimo",
   },
   maxLength: {
-    value: 20,
-    message: "Password debe contener 20 carácteres de máximo"
+    value: 8,
+    message: "El Password debe contener 8 carácteres como máximo",
   },
-}
-
+};
 
 export default function Home() {
-
-  const { handleSubmit, register, formState: { errors } } = useForm()
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
 
   const onSubmit = handleSubmit((data) => {
     loginUser(false, data)
-    .then((response)=> {
-     if(response) {
-      alert("login correcto")
-     } else {
-      alert("login incorrecto")
-     }
-    })
-    .catch((err)=> {
-      console.log(err)
-    })
-  })
-
-  console.log(errors)
+      .then((response) => {
+        if (response) {
+          alert("login correcto");
+        } else {
+          alert("login incorrecto");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 
   return (
     <form onSubmit={onSubmit} className={styles.containerForm}>
-      <InputLabel titleLabel={"Email"} htmlFor={"email"} register={register} validation={validationEmail} errors={errors.email} />
-      <InputLabel titleLabel={"Password"} htmlFor={"password"} register={register} validation={validationPassword} errors={errors.password} />
+      <InputContainer title={"Email"}>
+        <Input type="email" {...register("email"), validationEmail} errorsMessage={errors?.email.message} />
+      </InputContainer>
+      <InputContainer title={"Password"}>
+        <Input type="password" {...register("password"), validationPassword}  errorsMessage={errors?.password.message}/>
+      </InputContainer>
       <Button />
     </form>
-
   );
 }
--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -1,27 +1,60 @@
-"use client"
-import { useContext } from "react"
-import { useForm } from "react-hook-form";
-import { InputView } from "./InputView"
-import { InputContext } from "@/context/input"
-import { FaLock, FaCheck } from "react-icons/fa6";
-
+import { FaEye} from "react-icons/fa6";
+import styles from "./Input.module.scss";
+import {FC, InputHTMLAttributes, forwardRef} from "react";
 
 interface InputProps {
-  htmlFor : string;
-  register: any;
-  validation: any;
-  errors: any
-}
+  type?: string;
+  errorsMessage?: string;
+} 
 
-export const Input = ({htmlFor, register, validation, errors}:InputProps) => {
+export const Input = forwardRef<HTMLInputElement, InputProps>( ({
+  type = "text",
+  errorsMessage,
+  ...props 
+}: InputProps, ref) => {
 
-    const {inputState}= useContext(InputContext)
+  return (
+    <div className={styles.containerInputError}>
+      <div className={styles.containerInput}>
+        <input 
+          {...props}
+          ref={ref}
+          type={type}
+          className={styles.input}
+        />
+        <span className={styles.children}>{type === "password" && <FaEye/>}</span>
+      </div>
+      {!!errorsMessage && <span className={styles.error}>{errorsMessage}</span>}
+    </div>
+  );
+});
 
-    return (
-              <InputView type={"text"} errors={errors} validation={validation} register={register} htmlFor={htmlFor} textPlaceHolder={inputState.placeholder} styleInput={inputState.styleInput}>
-                {inputState.placeholder === "Left Component" && <FaCheck/>}
-                {inputState.placeholder === "Secured" &&  <FaLock/>}
-              </InputView>
-    )
-}
+
+
+// "use client"
+// import { useContext } from "react"
+// import { InputView } from "./InputView"
+// import { InputContext } from "@/context/input"
+// import { FaEye} from "react-icons/fa6";
+// import { type } from "os";
+
+
+// interface InputProps {
+//   htmlFor : string;
+//   register: any;
+//   validation: any;
+//   errors: any;
+//   type: string
+// }
+
+// export const Input = ({htmlFor, register, validation, errors, type}:InputProps) => {
+
+//     const {inputState}= useContext(InputContext)
+
+//     return (
+//               <InputView type={type} errors={errors} validation={validation} register={register} htmlFor={htmlFor} textPlaceHolder={inputState.placeholder} styleInput={inputState.styleInput}>
+//                
+//               </InputView>
+//     )
+// }
 
